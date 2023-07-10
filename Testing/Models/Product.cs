80a2af2a6b6a43dd3f455fdd913b5789b1b8a57e
Henry (Hendra) Gozali <123764602+goz88hen@users.noreply.github.com>
2023-07-10 15:33:49 -0700

    Completed exercise 4.

--- Testing/Models/Product.cs
+++ Testing/Models/Product.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace Testing.Models
 {
@@ -13,7 +14,7 @@ namespace Testing.Models
         public int CategoryID { get; set; }
         public int OnSale { get; set; }
         public int StockLevel { get; set; }
-
+        public IEnumerable<Category> Categories { get; set; }
     }
 }
 
